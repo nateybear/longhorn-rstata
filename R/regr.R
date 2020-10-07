5499d2b99cb5c483df849d81b59956b336347153
Nathan Hattersley <nate.hattersley@gmail.com>
2020-10-07 13:43:05 -0500

    add b_ function to index coefficients. tidy with styler.

--- R/regr.R
+++ R/regr.R
@@ -5,21 +5,35 @@
 #' Run a regression and store it globally like in Stata. Aims to support as many
 #' features as Stata does (robust standard errors, clustering, confidence levels...)
 #'
-#'@details
+#' @details
 #' TODO: add more description to this. It will need to be one of the heavy lifters.
 #' TODO: create a class for this that extends lm functionality
 #'
 #' @param formula a formula using variables from the loaded dataset
 #'
-#' @examples
+#' @example
 #' use(datasets::mtcars)
 #' regr(mpg ~ wt + disp*cyl)
 #'
 #' @export
-regr <- function(formula) { # TODO accept arguments for robust/cluster and level
-    model <- stats::lm(formula, data = .data())
-    assign("model", model, envir = .rstata_env)
-    print(summary(model)) # TODO quick and dirty, use class behavior
+regr <- function(formula) { # TODO accept arguments for robust/cluster and
+  model <- stats::lm(formula, data = .data())
+  assign("model", model, envir = .rstata_env)
+  print(summary(model)) # TODO quick and dirty, use class behavior
 
-    invisible(NULL)
+  invisible(NULL)
 }
+
+#' @title
+#' Extract Coefficient Point Estimates
+#' @details
+#' After fitting model using regr, use this to extract point estimates of the
+#' coefficients like in Stata.
+#' @example
+#' use(datasets::mtcars)
+#' regr(mpg ~ wt)
+#' print(b_(wt))
+#'
+#'
+#' @export
+b_ <- function(v) stats::coef(.model())[deparse(substitute(v))]
