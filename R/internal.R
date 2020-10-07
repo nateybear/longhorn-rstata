476d58610737c801ba80c98d97ca90bc36a465d5
Nathan Hattersley <nate.hattersley@gmail.com>
2020-10-07 13:36:38 -0500

    modify use to clear search path properly (and clear existing model)

--- R/internal.R
+++ R/internal.R
@@ -1,25 +1,29 @@
 # These functions shall not be exported.
 
-.rstata_env = new.env()
+.rstata_env <- new.env()
 
-.model_check <- function() {
-    if (!exists("model", envir = .rstata_env)) {
-        stop("There is not regression model in the environment.\nPlease use regr() first.")
+.check <- function(var, errMsg = sprintf("%s does not exist", var), silent = FALSE) {
+  if (!exists(var, envir = .rstata_env)) {
+    if (!silent) {
+      stop(errMsg)
+    } else {
+      FALSE
     }
+  } else {
+    TRUE
+  }
 }
 
-.model <- function() {
-    .model_check()
-    get("model", envir = .rstata_env)
+.get <- function(var, silent = FALSE, checkFun = function() .check(var, silent = silent)) {
+  if (checkFun()) {
+    get(var, envir = .rstata_env)
+  }
 }
 
-.data_check <- function() {
-    if (!exists("data", envir = .rstata_env)) {
-        stop("There is no loaded dataset.\nPlease use load() first.")
-    }
-}
+.model_check <- function() .check("model", "There is not regression model in the environment.\nPlease use regr() first.")
+.model <- function() .get("model", checkFun = .model_check)
 
-.data <- function() {
-    .data_check()
-    get("data", envir = .rstata_env)
-}
+.data_check <- function() .check("data", "There is no loaded dataset.\nPlease use load() first.")
+.data <- function() .get("data", checkFun = .data_check)
+
+printf <- function(...) cat(sprintf(...))
--- R/use.R
+++ R/use.R
@@ -2,22 +2,54 @@
 #' Load a dataset for use with rstata
 #'
 #'
-#'@details
+#' @details
 #' Load either a specified dataset or browse your computer for a .dta file
 #'
 #' @param dataset optional, attach a dataframe already in your R workspace
 #'
 #' @examples
 #' # suggested usage: browse to a .dta file and use
-#' \dontrun{use()}
+#' \dontrun{
+#' use()
+#' }
 #'
 #' # alternative usage: re-use a dataframe already in R
 #' use(datasets::mtcars)
-#'
 #' @export
-use <- function(dataset=NULL) {
-    if (is.null(dataset)) {
-        dataset <- foreign::read.dta(file.choose())
-    }
-    assign("data", dataset, envir = .rstata_env)
+use <- function(dataset = NULL) {
+  if (.check("data_name", silent = TRUE)) {
+    old_data <- .get("data_name")
+    try({
+        detach(old_data, character.only = TRUE)
+        printf("Detached old data %s.\n\n", old_data)
+    }, silent = TRUE)
+  }
+
+  if (is.null(dataset)) {
+    fname <- file.choose()
+    dataset <- foreign::read.dta(fname)
+    data_name <- path_to_name(fname)
+  } else {
+    data_name <- deparse(substitute(dataset))
+  }
+
+  attach(dataset, name = data_name)
+
+  if (.check("model", silent = TRUE)) {
+    rm("model", envir = .rstata_env)
+  }
+  assign("data", dataset, envir = .rstata_env)
+  assign("data_name", data_name, envir = .rstata_env)
+
+  printf("Using data %s. It is attached in your workspace, try typing \"summary(%s)\".\n", data_name, names(dataset)[1])
+  printf("Choose %s from the dropdown in the Environment tab to view the data.\n", data_name)
+}
+
+
+# helper to name our datasets
+path_to_name <- function(path) {
+  path <- normalizePath(path, "/")
+  split_path <- unlist(strsplit(path, "/"))
+  data_set <- split_path[length(split_path)]
+  unlist(strsplit(data_set, "([.]DTA)|([.]dta)"))[1]
 }
